b6ce48057c2f8201ca4b6813d58f3df4037111b8
Josh Holland <josh@inv.alid.pw>
2021-01-12 22:36:18 +0000

    wip fix for todos in fs.getAppDataDir

--- lib/std/fs/get_app_data_dir.zig
+++ lib/std/fs/get_app_data_dir.zig
@@ -15,6 +15,39 @@ pub const GetAppDataDirError = error{
     AppDataDirUnavailable,
 };
 
+const HomeDirError = error{
+    /// Couldn't determine user information
+    UserInfoUnavailable,
+    /// Couldn't parse /etc/passwd
+    EtcPasswdUnreadable,
+    /// No matching user was found in /etc/passwd
+    UserNotFound,
+    OutOfMemory,
+};
+fn homeDirFromPasswd(allocator: *mem.Allocator) HomeDirError![]u8 {
+    const user = os.getenv("USER");
+    const uid = if (builtin.os.tag == .linux) os.linux.getuid() else null;
+    if (user == null and uid == null) return error.UserInfoUnavailable;
+    var passwdFile = fs.openFileAbsolute("/etc/passwd", .{}) catch return error.EtcPasswdUnreadable;
+    defer passwdFile.close();
+    const r = passwdFile.reader();
+
+    while (try r.readUntilDelimiterAlloc(allocator, '\n')) |line| : (allocator.free(line)) {
+        errdefer allocator.free(line);
+        var fields = mem.split(line, ":");
+        const login = fields.next() orelse return error.EtcPasswdUnreadable;
+        if (user != null and !mem.eql(u8, user.?, login)) continue;
+        _ = fields.next() orelse return error.EtcPasswdUnreadable; // password
+        const uid_str = fields.next() orelse return error.EtcPasswdUnreadable;
+        const line_uid = fmt.parseInt(u32, uid_str, 10) catch return error.EtcPasswdUnreadable;
+        if (uid != null and uid.? != line_uid) continue;
+        // we've now definitely found the right line
+        _ = fields.next() orelse return error.EtcPasswdUnreadable; // group id
+        _ = fields.next() orelse return error.EtcPasswdUnreadable; // user name/comment
+        return fields.next() orelse return error.EtcPasswdUnreadable;
+    } else return error.UserNotFound;
+}
+
 /// Caller owns returned memory.
 /// TODO determine if we can remove the allocator requirement
 pub fn getAppDataDir(allocator: *mem.Allocator, appname: []const u8) GetAppDataDirError![]u8 {
@@ -43,17 +76,13 @@ pub fn getAppDataDir(allocator: *mem.Allocator, appname: []const u8) GetAppDataD
             }
         },
         .macos => {
-            const home_dir = os.getenv("HOME") orelse {
-                // TODO look in /etc/passwd
-                return error.AppDataDirUnavailable;
-            };
+            // XXX we need to free if homeDirFromPasswd is used (but not if getenv succeeded)
+            const home_dir = os.getenv("HOME") orelse homeDirFromPasswd(allocator) catch return error.AppDataDirUnavailable;
             return fs.path.join(allocator, &[_][]const u8{ home_dir, "Library", "Application Support", appname });
         },
         .linux, .freebsd, .netbsd, .dragonfly, .openbsd => {
-            const home_dir = os.getenv("HOME") orelse {
-                // TODO look in /etc/passwd
-                return error.AppDataDirUnavailable;
-            };
+            // XXX need to free as above
+            const home_dir = os.getenv("HOME") orelse homeDirFromPasswd(allocator) catch return error.AppDataDirUnavailable;
             return fs.path.join(allocator, &[_][]const u8{ home_dir, ".local", "share", appname });
         },
         else => @compileError("Unsupported OS"),
